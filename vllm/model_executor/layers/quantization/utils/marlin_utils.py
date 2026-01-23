9b07a95f000b94c5cedaaaa512c7729d732c4a8e
Michael Goin <mgoin64@gmail.com>
2026-01-23 16:48:12 -0500

    [Refactor] Rename `gptq_marlin` to `marlin` to match MoE (#32952)
    Signed-off-by: mgoin <mgoin64@gmail.com>

--- vllm/model_executor/layers/quantization/utils/marlin_utils.py
+++ vllm/model_executor/layers/quantization/utils/marlin_utils.py
@@ -567,7 +567,7 @@ def apply_gptq_marlin_linear(
 
         reshaped_x, a_scales = marlin_quant_input(reshaped_x, input_dtype)
 
-    output = ops.gptq_marlin_gemm(
+    output = ops.marlin_gemm(
         reshaped_x,
         None,
         weight,
@@ -632,7 +632,7 @@ def apply_awq_marlin_linear(
         )
         reshaped_x, a_scales = marlin_quant_input(reshaped_x, input_dtype)
 
-    output = ops.gptq_marlin_gemm(
+    output = ops.marlin_gemm(
         reshaped_x,
         None,
         weight,
